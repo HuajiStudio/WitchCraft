a796ca94da1fa547618954c7ce232f37376b548e
Lasm Gratel <lasm_gratel@hotmail.com>
2017-08-26 21:15:24 +0800

    fixed wand recipe

--- src/main/java/huajistudio/witchcraft/crafting/RecipesWands.java
+++ src/main/java/huajistudio/witchcraft/crafting/RecipesWands.java
@@ -2,25 +2,23 @@ package huajistudio.witchcraft.crafting;
 
 import huajistudio.witchcraft.WitchCraft;
 import huajistudio.witchcraft.item.ItemLoader;
-import huajistudio.witchcraft.util.Namer;
-import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.ResourceLocation;
-import net.minecraftforge.fml.common.registry.GameRegistry;
-import net.minecraftforge.oredict.OreDictionary;
+import net.minecraftforge.fml.common.registry.ForgeRegistries;
+import net.minecraftforge.oredict.ShapedOreRecipe;
 
 class RecipesWands {
 	private final String[] recipePattern = new String[]{"#X#", " W ", " W "};
 
 	void addRecipes() {
-		ItemLoader.WAND_MAP.forEach((key, value) -> OreDictionary.getOres("gemMagicCrystal").forEach(gemEntry ->
-			GameRegistry.addShapedRecipe(
-				new ResourceLocation(WitchCraft.MODID, Namer.buildUnlocalizedName("wand", key.name())),
-				new ResourceLocation(WitchCraft.MODID, "wand"),
-				new ItemStack(value), recipePattern,
-				'#', key.getRepairItemStack(),
-				'X', gemEntry.getItem(),
-				'W', Items.STICK)
-        ));
+		ItemLoader.WAND_MAP.forEach((key, value) ->
+			ForgeRegistries.RECIPES.register(new ShapedOreRecipe(new ResourceLocation("misc"),
+					new ItemStack(value), recipePattern,
+					'#', key.getRepairItemStack(),
+					'X', "gemMagicCrystal",
+					'W', "stickWood")
+					.setRegistryName(new ResourceLocation(WitchCraft.MODID, "wand" + "_" + key.name()))
+			)
+		);
 	}
 }
